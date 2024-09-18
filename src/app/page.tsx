72ad0e06eeec4a3459686d33cdd040837906f5c3
Azarudeen <azarudeendanish@gmail.com>
2024-09-18 17:51:11 +0530

    text updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div style={{ height: '100vh', display: 'flex',flexDirection: 'column', justifyContent: 'center', alignItems: 'center', background: 'linear-gradient(red, yellow)' }}><div className="text-[100px]">Welcome to Admin Panel</div>
+      <div style={{ height: '100vh', display: 'flex',flexDirection: 'column', justifyContent: 'center', alignItems: 'center', background: 'linear-gradient(red, yellow)' }}><div className="text-[50px] text-center">Welcome to Admin Panel</div>
         <Link className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           href="AddProducts">
           Add New Products
